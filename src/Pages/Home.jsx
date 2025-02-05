c0a4b26829a478a580583279f902503740f86424
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-02-05 09:26:52 +0530

    Added r

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import './Home.css'
-import Header from '../Components/Home/Header'
 import About from '../Components/Home/about'
 import Services from '../Components/Home/service'
 import CaseStudies from '../Components/Home/case-study'
@@ -10,7 +9,6 @@ import Video from '../Components/Home/video'
 import ChooseUs from '../Components/Home/chooseUs'
 import Contact from '../Components/Home/contact'
 import Testimonials from '../Components/Home/testimonal'
-import Footer from '../Components/Home/footer'
 
 import Hero from '../Components/Home/Hero'
 
