2e8b60417cf54d5df8e2d1251ded87274a306b09
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-02-25 20:06:09 +0530

    dfg

--- src/Components/Common/checkAuth.jsx
+++ src/Components/Common/checkAuth.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from "react-router-dom";
 
-const checkAuth = ({ isAuthenticated, user, children }) => {
+function CheckAuth({ isAuthenticated, user, children }) {
   const location = useLocation();
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  } else {
-    if (user?.role === 'admin') {
-      return children;
-    } else if (user?.role === 'client') {
-      return <Navigate to="/client/dashboard" replace />;
+  console.log(location.pathname, isAuthenticated);
+
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
+      return <Navigate to="/auth/login" />;
     } else {
-      return <Navigate to="/unauthorized" replace />;
+      if (user?.role === "admin") {
+        return <Navigate to="/admin/dashboard" />;
+      } else {
+        return <Navigate to="/shop/home" />;
+      }
     }
   }
-};
 
-export default checkAuth;
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  return <>{children}</>;
+}
+
+export default CheckAuth;
